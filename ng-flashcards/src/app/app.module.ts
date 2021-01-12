538ced2174a591b52056aaab359e8cf0bdaf612a
Joseph <nguld@itsj-group.com>
2021-01-12 15:58:10 +0700

    updated ng-flashcards

--- ng-flashcards/src/app/app.module.ts
+++ ng-flashcards/src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {FlashComponent} from './components/flash/flash.component';
+import {FormsModule} from "@angular/forms";
+import {FlashService} from './services/flash.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlashComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
-  providers: [],
+  providers: [FlashService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ ng-flashcards/src/app/components/flash/flash.component.ts
@@ -0,0 +1,55 @@
+import {ChangeDetectionStrategy, Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+import {IFlash} from '../../models/flash.model';
+
+@Component({
+  selector: 'app-flash',
+  templateUrl: './flash.component.html',
+  styleUrls: ['./flash.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FlashComponent implements OnInit {
+
+  @Input() flash: IFlash = {
+    question: '',
+    answer: '',
+    id: 0,
+    show: false,
+  };
+  @Output() toggleCard = new EventEmitter();
+  @Output() delete = new EventEmitter();
+  @Output() edit = new EventEmitter();
+  @Output() rememberedChange = new EventEmitter();
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onToggleCard() {
+    this.toggleCard.emit(this.flash.id);
+  }
+
+  deleteFlash() {
+    this.delete.emit(this.flash.id);
+  }
+
+  editFlash() {
+    this.edit.emit(this.flash.id);
+  }
+
+  markCorrect() {
+    this.rememberedChange.emit({
+      id: this.flash.id,
+      flag: 'correct'
+    });
+  }
+
+  markIncorrect() {
+    this.rememberedChange.emit({
+      id: this.flash.id,
+      flag: 'incorrect'
+    });
+  }
+
+}
